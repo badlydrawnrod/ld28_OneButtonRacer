4914a5db5e1ab1eb56fbcca1d8659e6f0f0b2960
Rod Hyde <rod@badlydrawngames.com>
2013-12-27 18:10:12 +0000

    States are now responsible for clearing the screen.

--- ld28/src/ld28/Playing.java
+++ ld28/src/ld28/Playing.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Playing extends State {
@@ -90,6 +91,10 @@ public class Playing extends State {
 
 	@Override
 	public void draw() {
+		Gdx.gl.glDisable(GL10.GL_SCISSOR_TEST);
+		Gdx.gl.glClearColor(0.0f, 0.2f, 0.0f, 1.0f);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
 		worldRenderer.draw();
 		guiCam.activate();
 		
